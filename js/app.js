a985e8460b26261830a72e27d1436815ed29921c
ariel.aquino <aquinoariel@hotmail.com>
2021-09-25 14:01:47 -0300

    ejercicios

---
+++ js/app.js
@@ -0,0 +1,48 @@
+/*let numero = 2
+let num = 1
+
+console.log(numero + num)*/
+
+/*let numero1 = "5"
+let num2 = 10
+
+console.log(parseInt(numero1) + num2)*/
+
+
+/*let numero1 ="5"
+let num2 = 10
+
+alert(parseInt(numero1) + num2)*/
+
+/*let moneda ="pesos"
+let comida ="guiso"
+
+alert(`con ${10 + 5} ${moneda} me hago alto ${comida}`)*/
+
+/*let saludo = "Hola"
+let nombre = prompt(`ingrese su nombre`)
+let apellido = prompt(`ingrese su apellido`)
+let estado = ", que tengas un buen día"
+
+alert(saludo + nombre + apellido + estado)*/
+
+/*let edad = prompt(`ingrese su edad`)
+let respuesta = 18
+
+if(edad < respuesta){
+    alert("sos menor de edad")
+}else if(edad >= respuesta){
+    alert("sos mayor de edad")
+}*/
+
+let argentina = prompt(`ingrese puesto de la selección Argentina`)
+let respuesta = "primero"
+
+if(argentina == respuesta){
+    alert("entonces brasil segundo")
+}else{
+    alert("te equivocas, es argentina primero y brasil segundo")
+}
+
+
+
